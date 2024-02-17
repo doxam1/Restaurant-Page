baba2307c3d497024c32cf5db602d8758f6500d6
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-02-17 07:07:44 +0200

    added grid to menu

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,14 @@
 export default function menuPage() {  
     const content = document.querySelector('#content');
-    content.innerHTML = '<h1>this is my test!<h1>';
+    content.innerHTML = 
+                        `<h1>Our Menu</h1>
+                        <div class='menuContainer'>
+                          <div class='item'><img src='img/soupimg.jpg' width='20%'><p> chicken soup <br> 3.5$</p></div>
+                          <div class='item'><p>bla</p></div>
+                          <div class='item'><p>bla</p></div>
+                          <div class='item'><p>bla</p></div>
+                          <div class='item'><p>bla</p></div>
+                          <div class='item'><p>bla</p></div>                   
+                        
+                        </div>`
 }
